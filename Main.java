5cb21d4546d5205f16cb16b3d9510f0d1908fa22
YujiSoftware <yuji.software+github@gmail.com>
2022-08-23 21:24:44 +0900

    Update sample code

--- Main.java
+++ Main.java
@@ -2,16 +2,21 @@ import jdk.incubator.foreign.*;
 import static jdk.incubator.foreign.ValueLayout.*;
 
 import java.lang.invoke.MethodHandle;
+import java.lang.invoke.VarHandle;
 
 public class Main {
-    private static GroupLayout GO_STRING = MemoryLayout.structLayout(
-            ADDRESS.withName("p"),
-            JAVA_LONG.withName("n"));
+    private static final OfAddress POINTER = ADDRESS.withName("p");
+    private static final OfLong NUMBER = JAVA_LONG.withName("n");
+
+    private static final GroupLayout GO_STRING = MemoryLayout.structLayout(POINTER, NUMBER);
+
+    private static final VarHandle GO_STRING_P = GO_STRING.varHandle(PathElement.groupElement(POINTER.name().get()));
+    private static final VarHandle GO_STRING_N = GO_STRING.varHandle(PathElement.groupElement(NUMBER.name().get()));
 
     public static void main(String[] args) throws Throwable {
         System.loadLibrary("gopher");
 
-        send("Hello Gopher!");
+        send("Hello Gopher! (from Duke)");
     }
 
     private static void send(String message) throws Throwable {
@@ -24,8 +29,8 @@ public class Main {
         try (ResourceScope scope = ResourceScope.newConfinedScope()) {
             MemorySegment offHeap = MemorySegment.allocateNative(GO_STRING, scope);
             MemorySegment cString = SegmentAllocator.implicitAllocator().allocateUtf8String(message);
-            offHeap.set(ValueLayout.ADDRESS, 0, cString);
-            offHeap.set(ValueLayout.JAVA_LONG, 8, cString.byteSize());
+            GO_STRING_P.set(offHeap, cString.address());
+            GO_STRING_N.set(offHeap, cString.byteSize());
 
             recv.invoke(offHeap);
         }
